5149040ee8ce3429e7dad09c377da5fde4a5b51a
Brian Sheedy <weableandbob@gmail.com>
2016-05-08 21:50:23 -0400

    Updated c code to properly un-order the matrix

--- src/alpha_sortedness.c
+++ src/alpha_sortedness.c
@@ -10,12 +10,7 @@ int* genAlphaSortedArray(float alpha, int n){
     int num_replace = (int)((1 - alpha) * n);
     for(i = 0; i < num_replace; i++){
         int index = (int)(rand() % n);
-        if(index == 0){
-            array[index] = array[index + 1] + 1;
-        }
-        else{
-            array[index] = array[index - 1] - 1;
-        }
+        array[index] = n + 1;
     }
     return array;
 }
@@ -25,13 +20,12 @@ int* getRandomSubArrayLocal(int* array, int gamma, int n){
     return &array[index];
 }
 
-int* getRandomSubArrayFile();
-
-float calcAlphaSortedness(int* sub_array, int gamma){
+float calcAlphaSortedness(int* sub_array, int gamma, int n){
+    (void)n;
     int i;
     int num_unsorted = 0;
     for(i = 0; i < gamma - 1; i++){
-        if(sub_array[i] > sub_array[i+1]){
+        if(sub_array[i] >= sub_array[i + 1]){
             num_unsorted++;
         }
     }
@@ -40,11 +34,11 @@ float calcAlphaSortedness(int* sub_array, int gamma){
 
 int main(int argc, char* argv[]){
     time_t t;
-    int i;
+    int i, rep;
     srand((unsigned int) time(&t));
 
-    if(argc != 6){
-        printf("Usage is %s array_size alpha beta gamma array_alpha\n", argv[0]);
+    if(argc != 7){
+        printf("Usage is %s array_size alpha beta gamma array_alpha num_repetitions\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
@@ -53,21 +47,23 @@ int main(int argc, char* argv[]){
     int beta = atoi(argv[3]);
     int gamma = atoi(argv[4]);
     float alpha_prime = atof(argv[5]);
+    int num_repetitions = atoi(argv[6]);
 
     int* array = genAlphaSortedArray(alpha_prime, n);
-
-    float total_alpha = 0;
-    for(i = 0; i < beta; i++){
-        int* sub_array = getRandomSubArrayLocal(array, gamma, n);
-        total_alpha += calcAlphaSortedness(sub_array, gamma);
+    int successes = 0;
+    for(rep = 0; rep < num_repetitions; rep++){
+        float total_alpha = 0;
+        for(i = 0; i < beta; i++){
+            int* sub_array = getRandomSubArrayLocal(array, gamma, n);
+            total_alpha += calcAlphaSortedness(sub_array, gamma, n);
+        }
+        float approx_alpha = total_alpha / beta;
+        if(approx_alpha >= alpha){
+            successes += 1;
+        }
     }
-    float approx_alpha = total_alpha / beta;
-    if(approx_alpha >= alpha){
-        printf("True\n");
-    }
-    else{
-        printf("False\n");
-    }
+    
+    printf("Succeeded %d out of %d times (%4f)\n", successes, num_repetitions, (1.0 * successes) / num_repetitions);
 
     free(array);
     return 0;
